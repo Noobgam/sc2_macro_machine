5569e05171d0200e0f8ac215ba90a00241bf8521
Noobgam <shessmaster12@gmail.com>
2020-08-09 12:38:27 +0000

    submodule

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+VERBOSE=1 cmake --build . --target main_bot
--- build_meta_calculator.sh
+++ build_meta_calculator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir build
-cd build || exit
+cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 VERBOSE=1 cmake --build . --target meta_calculator
