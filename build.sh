98c9397384e6ec4ecb3d82e1144c652d7cc24122
Noobgam <shessmaster12@gmail.com>
2020-08-13 08:02:08 +0000

    asan in build

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-VERBOSE=1 cmake --build . --target meta_calculator
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+VERBOSE=1 cmake --build . --target main_bot
