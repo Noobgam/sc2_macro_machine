5ab3e76080476d7726b529b5dd590e9387be3032
Noobgam <shessmaster12@gmail.com>
2020-08-22 14:26:58 +0000

    entrypoint and push

--- infra/tests/update-latest.sh
+++ infra/tests/update-latest.sh
@@ -1,3 +1,3 @@
 sudo docker build -t sc2-macro-machine-autotest .
 sudo docker tag sc2-macro-machine-autotest:latest 928383298076.dkr.ecr.eu-central-1.amazonaws.com/sc2-macro-machine-autotest:latest
-#sudo docker push 928383298076.dkr.ecr.eu-central-1.amazonaws.com/sc2-macro-machine-autotest:latest
+sudo docker push 928383298076.dkr.ecr.eu-central-1.amazonaws.com/sc2-macro-machine-autotest:latest
