3c93cd32cfc52cb5910cab841b8122c181fed7fd
xrater <alkira314@gmail.com>
2020-08-25 03:06:20 +0300

    Do not give orders onStart

--- bot/micro/order/workers/CollectMineralsOrder.cpp
+++ bot/micro/order/workers/CollectMineralsOrder.cpp
@@ -17,7 +17,7 @@ void CollectMineralsOrder::onStart() {
         BOT_ASSERT(unit->getType().isWorker(), "Can harvest minerals only with workers.");
     }
 #endif
-    assignWorkers(m_squad->units());
+    m_unassignedWorkers.insert(m_unassignedWorkers.end(), m_squad->units().begin(), m_squad->units().end());
 }
 
 void CollectMineralsOrder::onStep() {
@@ -55,18 +55,17 @@ void CollectMineralsOrder::onStep() {
     }
 
     // reassign workers
-    std::set<const Unit*> unassignedWorkers;
     for (auto& mineralID : toDelete) {
         const auto& ptr = m_mineralToWorker.find(mineralID);
         BOT_ASSERT(ptr != m_mineralToWorker.end(), "Mineral was not found");
-        unassignedWorkers.insert(ptr->second.begin(), ptr->second.end());
+        m_unassignedWorkers.insert(m_unassignedWorkers.end(), ptr->second.begin(), ptr->second.end());
     }
-    assignWorkers(unassignedWorkers);
+    assignWorkers();
 }
 
 void CollectMineralsOrder::onUnitAdded(const Unit *unit) {
     BOT_ASSERT(unit->getType().isWorker(), "Can harvest minerals only with workers.");
-    assignWorkers({unit});
+    m_unassignedWorkers.emplace_back(unit);
 }
 
 void CollectMineralsOrder::onUnitRemoved(const Unit *unit) {
@@ -80,26 +79,15 @@ void CollectMineralsOrder::onUnitRemoved(const Unit *unit) {
     }
 }
 
-void CollectMineralsOrder::assignWorkers(const std::set<const Unit *>& workers) {
-    if (!workers.empty()) {
-        auto &&logLine = LOG_DEBUG << "About to assign workers to minerals: ";
-        for (auto &worker : workers) {
-            if (!worker->isValid()) {
-                logLine << "null ";
-            } else {
-                logLine << worker->getID() << " ";
-            }
-        }
-        logLine << BOT_ENDL;
-    }
+void CollectMineralsOrder::assignWorkers() {
     auto& minerals = m_base->getBaseLocation()->getMinerals();
-    for (auto& worker : workers) {
+    for (auto workerIt = m_unassignedWorkers.begin(); workerIt != m_unassignedWorkers.end();) {
         auto bestIt = std::min_element(m_mineralToWorker.begin(), m_mineralToWorker.end(),
             [](const auto& mw1, const auto& mw2) {
                 return mw1.second.size() < mw2.second.size();
             }
         );
-        bestIt->second.emplace_back(worker);
+        bestIt->second.emplace_back(*workerIt);
         auto id = bestIt->first;
         const auto& mineralIt = std::find_if(minerals.begin(), minerals.end(),
             [id](const auto& m) {
@@ -107,12 +95,16 @@ void CollectMineralsOrder::assignWorkers(const std::set<const Unit *>& workers) 
             }
         );
         if (mineralIt != minerals.end()) {
-            worker->gatherMineral(*(*mineralIt)->getUnit());
+            LOG_DEBUG << "Worker " << (*workerIt)->getID() << " was assigned on mineral " << (*mineralIt)->getID() << BOT_ENDL;
+            (*workerIt)->gatherMineral(*(*mineralIt)->getUnit());
+            workerIt = m_unassignedWorkers.erase(workerIt);
         } else {
             onEnd();
+            break;
         }
     }
 }
+
 void CollectMineralsOrder::fixWorker(const Unit *worker, const Resource* mineral) const {
     LOG_DEBUG << "Fixing worker " << worker->getID() << BOT_ENDL;
     if (worker->carriesResources()) {
